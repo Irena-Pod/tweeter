0fb3c9be84c990b88990893e8db9429b5c565902
Irena-Pod <irena.podgurski@gmail.com>
2020-12-15 20:15:16 +0000

    add counter feature

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  // Coutner turns red when goes into he negative
+  $("#tweet-text").on("input", function () {
+    const maxLength = 140;
+    let currentLength = $(this).val().length;
+    const difference = maxLength - currentLength;
+
+    $(".counter").text(difference);
+
+    if (difference < 0) {
+      $(".counter").addClass("negative");
+    } else {
+      $(".counter").removeClass("negative");
+    }
+  });
+});
